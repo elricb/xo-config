d4b5604b4a001b609bcc9ba15095f5e39d312aa7
Elric Best <elricb@users.noreply.github.com>
2021-10-01 00:07:10 -0400

    initial commit

---
+++ .xo-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "space": true,
+  "prettier": true,
+  "extends": ["xo-react"],
+  "plugins": ["react"],
+  "rules": {
+    "import/extensions": 0,
+    "node/file-extension-in-import": 0,
+    "react/jsx-sort-props": 0,
+    "react/no-array-index-key": 1,
+    "react/no-unused-prop-types": 1,
+    "react/prop-types": 1,
+    "unicorn/explicit-length-check": 0,
+    "unicorn/filename-case": 1,
+    "unicorn/no-array-reduce": 0,
+    "unicorn/no-for-loop": 0,
+    "unicorn/no-hex-escape": 0,
+    "unicorn/prefer-module": 0,
+    "unicorn/prefer-spread": 0
+  }
+};
---
+++ readme.md
@@ -0,0 +1,18 @@
+# @elric/xo-config
+
+Personal [xo](https://www.npmjs.com/package/xo) config for my projects.
+
+## Install
+
+```
+$ npm install --save-dev @elricb/xo-config
+```
+
+## Usage
+
+Add the below to the `.xo-config.js` file in your project.
+
+```js
+module.exports = require("@elric/xo-config");
+```
+
