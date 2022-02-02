0de80cc97c297d7ed6f925ccc25743ffd0a24fd7
Elric Best <elricb@users.noreply.github.com>
2022-02-01 23:47:19 -0500

    Changed - updated .xo-config.js reasons

--- .xo-config.js
+++ .xo-config.js
@@ -4,45 +4,45 @@ module.exports = {
   "extends": ["xo-react"],
   "plugins": ["react"],
   "rules": {
-    // i != 0 seems alright for string/int checking
+    // String + Int checking is standard practice; i != 0
     "eqeqeq": 0,
-    // in webpack no extension allows import of directory/index
+    // In webpack no extension allows import of directory/index
     "import/extensions": 0,
-    // checking equal/not-equal is valid logic
+    // Checking equal/not-equal is valid logic
     "no-negated-condition": 0,
-    // in webpack no extension allows import of directory/index
+    // In webpack no extension allows import of directory/index
     "node/file-extension-in-import": 0,
-    // doesn't make sense for CLI apps (e.g. verbose)
+    // Doesn't make sense for CLI apps (e.g. verbose)
     "react/boolean-prop-naming": 1,
-    // template + curl encodes HTML characters properly {`% @`}; always using looks cleaner as well
+    // Template + curly encodes HTML characters properly {`% @`}; Rule should be to always use curly braces
     "react/jsx-curly-brace-presence": 0,
     // JSX props in a specific order is beyond anal with no benefit
     "react/jsx-sort-props": 0,
-    // not always possible; don't always have control over 3rd party args
+    // Not always possible; don't always have control over 3rd party data
     "react/no-array-index-key": 1,
-    // should be a warning
+    // Should be a warning
     "react/no-unused-prop-types": 1,
-    // should be a warning
+    // Should be a warning
     "react/prop-types": 1,
-    // just changing to warning
+    // Should be a warning
     "react-hooks/exhaustive-deps": 1,
-    // greater/less-then are valid length checks
+    // Greater/less-then are valid length checks
     "unicorn/explicit-length-check": 0,
-    // i'm not 100% sold on this
+    // I'm not 100% sold on this ATM
     "unicorn/filename-case": 1,
-    // reduce is a performant/powerful method
+    // Reduce is a performant/powerful method
     "unicorn/no-array-reduce": 0,
-    // for loops are valid code instructions; adopted from c
+    // For loops are valid code instructions; adopted from c
     "unicorn/no-for-loop": 0,
-    // hex and unicode are both valid logic interfaces
+    // Hex and unicode are both valid logic interfaces
     "unicorn/no-hex-escape": 0,
     // __dirname/__filename are useful when used correctly in jobs (not everything is an app)
     "unicorn/prefer-module": 0,
-    // this isn't available in current node lts version
+    // This isn't available in current node lts version
     "unicorn/prefer-node-protocol": 0,
-    // spread everything isn't performant
+    // Spread everything isn't performant
     "unicorn/prefer-spread": 0,
-    // this doesn't always apply; it's better to carry standards through like stdio/stderr; also reserved words with underscore is poor practice
+    // It's preferable to carry standards through like stdio/stderr; reserved words circumvented with underscore is poor practice
     "unicorn/prevent-abbreviations": 1,
   }
 };
