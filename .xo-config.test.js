cc1ec6d93d39f05270e42928e4495416834ce1a3
Elric Best <elricb@users.noreply.github.com>
2022-08-06 16:49:41 -0400

    Added jest test for this config

---
+++ .xo-config.test.js
@@ -0,0 +1,9 @@
+const {existsSync} = require("fs");
+
+test(".xo-config.js exists", () => {
+  expect(existsSync("./.xo-config.js")).toBe(true);
+});
+
+test(".xo-config.js exports", () => {
+  expect(typeof require("./.xo-config.js")).toBe("object");
+});
